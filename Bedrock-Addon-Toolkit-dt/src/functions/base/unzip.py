e4a3042b215dcdf6628c54f96b7f213b56af1b78
DT-1992 <99299225+DT-1992@users.noreply.github.com>
2023-05-15 09:51:03 +0200

    Update unzip.py

--- Bedrock-Addon-Toolkit-dt/src/functions/base/unzip.py
+++ Bedrock-Addon-Toolkit-dt/src/functions/base/unzip.py
@@ -6,8 +6,6 @@ unzip  from   to
 """
 
 def unzip_from_to(path_in,path_out ):
-    name=files_in_path(path_in)
-    for root, path_in, files in os.walk(path_in):
-        if name in files:
+
             with ZipFile(path_in, 'r') as  zip_ref:
                 zip_ref.extractall(path_out)
